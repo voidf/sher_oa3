51dd707ba5dcb3abbb7e1d6b6b8c4fa6ca6fcbf4
voidf <510030805@qq.com>
2020-07-10 12:05:03 +0800

    s

--- backend/app/models/Sign.py
+++ backend/app/models/Sign.py
@@ -7,10 +7,10 @@ class Sign(db.Document):
     create_datetime = db.DateTimeField()  # 签到时间
     typ = db.StringField()  # 签到类型，分为'n'正常签到和's'换班签到
     week = db.IntField()  # 签到周
-    
-    user = db.ReferenceField(app.models.User.User,reverse_delete_rule=2)
+    print(dir(app.models))
+    user = db.ReferenceField(User.User,reverse_delete_rule=2)
 
-    def create(user: app.models.User.User, typ: str, week: int) -> bool:
+    def create(user: User.User, typ: str, week: int) -> bool:
         if user.last_sign:
             if user.last_sign.timestamp() / 7200 != datetime.datetime.now() / 7200:  # 卡两个小时内多次签到的情况
                 s = Sign(create_datetime=datetime.datetime.now(),
@@ -32,7 +32,7 @@ class Sign(db.Document):
     def get_by_id(id):
         return Sign(id=id).first()
 
-    def get_by_user(user:app.models.User.User) -> dict:
+    def get_by_user(user:User.User) -> dict:
         return {"signs":[i.get_base_info() for i in Sign.objects(user=user)]}
 
     def get_base_info(self) -> dict:
