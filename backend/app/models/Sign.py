73084261bc1ddbd1be62dbdec8d66d1917e2921c
voidf <510030805@qq.com>
2020-07-10 12:16:28 +0800

    ??

--- backend/app/models/Sign.py
+++ backend/app/models/Sign.py
@@ -7,7 +7,7 @@ class Sign(db.Document):
     create_datetime = db.DateTimeField()  # 签到时间
     typ = db.StringField()  # 签到类型，分为'n'正常签到和's'换班签到
     week = db.IntField()  # 签到周
-    
+    import app.models.User
     user = db.ReferenceField(app.models.User.User,reverse_delete_rule=2)
 
     def create(user: app.models.User.User, typ: str, week: int) -> bool:
@@ -43,4 +43,3 @@ class Sign(db.Document):
             "typ": self.typ,
             "user": self.user
         }
-import app.models.User
