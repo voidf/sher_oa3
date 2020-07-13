be76d71e27a54f6e0b4208b09a27d3808568d736
VFC <12@3.4>
2020-07-13 09:47:31 +0800

    d

--- backend/app/models/User.py
+++ backend/app/models/User.py
@@ -119,7 +119,7 @@ class Sign(db.Document):
 
     def create(user: User, typ: str, week: int) -> bool:
         if user.last_sign:
-            if user.last_sign.create_datetime.timestamp() / 7200 != datetime.datetime.now() / 7200:  # 卡两个小时内多次签到的情况
+            if int(user.last_sign.create_datetime.timestamp() / 7200) != int(datetime.datetime.now().timestamp() / 7200):  # 卡两个小时内多次签到的情况
                 s = Sign(create_datetime=datetime.datetime.now(),
                          typ=typ, week=week)
                 user.last_sign = s
